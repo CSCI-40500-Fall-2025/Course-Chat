5a471c8491071fb57c05bbb5641a31441ece1382
Shahzaib Fareed <shahzaibfareed1118@gmail.com>
2025-10-27 11:44:13 -0400

    frontend tests

--- web-frontend/setupTests.ts
+++ web-frontend/setupTests.ts
@@ -2,3 +2,13 @@ import "@testing-library/jest-dom";
 import { TextEncoder, TextDecoder } from 'util';
 
 Object.assign(global, { TextDecoder, TextEncoder });
+
+jest.mock("./src/contexts/AuthContext", () => ({
+    useAuth: () => ({
+      registerUser: jest.fn(() => Promise.resolve()),
+      loginUser: jest.fn(() => Promise.resolve()),
+    }),
+}));
+
+
+  
